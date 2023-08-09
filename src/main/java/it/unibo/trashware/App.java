56b922cc75fb2e5235011f856d0bb58c74fee44a
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-08-09 19:53:34 +0200

    add main class for the application

---
+++ src/main/java/it/unibo/trashware/App.java
@@ -0,0 +1,19 @@
+package it.unibo.trashware;
+
+/**
+ * This class provides the entry point for the application.
+ */
+public final class App {
+    
+    private App() {
+        // Empty
+    }
+
+    /**
+     * The entry point of the application.
+     * @param args unused
+     */
+    public static void main(final String... args) {
+        // TO DO
+    }
+}
